39292c436ad2f7ae5e5785fe414dbeab8fb525d9
Harry Li <harrylihongyi@gmail.com>
2013-01-14 14:30:54 +0800

    make basic User model and password

---
+++ backup files/static_pages_spec - Copy.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  let(:base_title){"Ruby on Rails Tutorial Sample App"}
+  subject { page }
+  
+  describe "Home page" do
+  	before(:each) { visit root_path }
+    
+	it { should have_selector('h1', :text => 'Sample App') }
+	it { should have_selector('title', :text => full_title('')) }
+	it { page.should_not have_selector('title', :text => '| Home') }
+  end
+  
+  describe "Help Page" do
+	before { visit help_path }
+
+	it { should have_selector('h1', :text => 'Help') }
+	it { should have_selector('title', :text => full_title('Help')) }
+  end
+  
+  describe "About Page" do
+  	before {visit about_path}
+
+	it { should have_selector('h1', :text => 'About Us') }	
+	it { should have_selector('title', :text => "#{base_title} | About Us") }
+  end
+  
+  describe "Contact Page" do
+  	before { visit contact_path }
+
+	it { should have_selector('h1', :text => 'Contact') }
+	it { should have_selector('title', :text => "#{base_title} | Contact") }
+  end
+  
+  
+end
